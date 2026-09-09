a5a74dae1208741e580a38b651897f9f7f48ed36
Kevin-TD <kdiggs0618@gmail.com>
2026-09-09 07:04:33 -0400

    some extra stuff

--- latex_parser/ast.ml
+++ latex_parser/ast.ml
@@ -1,4 +1,17 @@
 type expr =
   | Ident of string
+  | OperatorName of string
   | Number of float
-  
+  | Plus of expr * expr
+  | Minus of expr * expr
+  | Mul of expr * expr
+  | Div of expr * expr
+  | Exp of expr * expr
+  | Neg of expr
+
+  (* e.g., n=1 in a sum's subscript *)
+  | VarAssign of string * expr
+
+  (* a call to a function that has a subscript, superscript,
+  and body  *)
+  | OpCall3 of expr * expr * expr
